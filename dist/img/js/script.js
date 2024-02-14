13448c4eabe3073cf8f5a7e524cf37aac1ad9d9b
Mhdhafin <Dhafin1147@gmail.com>
2024-02-14 15:12:20 +0700

    first commit

---
+++ dist/img/js/script.js
@@ -0,0 +1,29 @@
+// Fixed
+window.onscroll = function () {
+  scrollFunction();
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+// Tombol top
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("back-to-top").style.display = "block";
+  } else {
+    document.getElementById("back-to-top").style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Lagu
